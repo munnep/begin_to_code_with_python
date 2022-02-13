4a72e4469baa6f7a94febebf5ae436c08db4b963
munnep <patrick.munne@hashicorp.com>
2022-02-13 11:54:29 +0100

    exercises chapter4

---
+++ chapter4/exercises/fahrenheit_to_degrees.py
@@ -0,0 +1,7 @@
+fahrenheit = input('Temprature Fahrenheit: ')
+
+fahrenheit_int = int(fahrenheit)
+
+degrees = (fahrenheit_int-32)/1.8
+
+print('Temprature in degrees', degrees)
---
+++ chapter4/exercises/strings.py
@@ -0,0 +1,15 @@
+print('it\'s a trap')
+print("it's a trap")
+print('''it's a trap''')
+
+
+print('''
+
+It works with multiple lines
+
+That is new to me
+
+''')
+
+
+print('Item\tSales\ncar\t50\nboat\t10')
